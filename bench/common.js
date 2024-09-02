54fdea6616fc2409eb6764ebef1f1795c56970f8
Laurin Brandner <mail@laurinbrandner.ch>
2024-09-02 10:18:31 +0200

    k6: random requests

--- bench/common.js
+++ bench/common.js
@@ -1,7 +1,7 @@
 import { check } from 'k6';
 import http from 'k6/http';
 import exec from 'k6/execution';
-import { randomString } from 'https://jslib.k6.io/k6-utils/1.3.0/index.js';
+import { randomString, randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.3.0/index.js';
 import { Counter } from 'k6/metrics';
 
 const payload_size = __ENV.PAYLOAD_SIZE || 1024;
@@ -15,7 +15,7 @@ const backends = [
 ];
 
 export function randomRequest() {
-  const server = __ENV.BACKEND || (exec.scenario.iterationInInstance) % 4 + 1;
+  const server = __ENV.BACKEND || randomIntBetween(1, 4);
   const signature = `server${server}`;
 
   var url = null;
