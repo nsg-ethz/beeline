737a966d41cdefcc1aa023eb6682f3b0395b0ef4
Laurin Brandner <mail@laurinbrandner.ch>
2024-09-02 10:18:04 +0200

    backend: add custom headers

--- exps/load-balancing/backend/src/main.rs
+++ exps/load-balancing/backend/src/main.rs
@@ -1,13 +1,15 @@
-use std::convert::Infallible;
-use std::net::ToSocketAddrs;
+use std::{convert::Infallible, net::ToSocketAddrs};
 
 use clap::Parser;
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::server::conn::http1;
+use hyper::header::HeaderValue;
+use hyper::{body::Body, server::conn::http1};
 use hyper::service::service_fn;
-use hyper::Response;
 use hyper_util::rt::TokioIo;
+use hyper::{
+    body::Incoming,
+    Request,
+    Response
+};
 use tokio::net::TcpListener;
 
 #[derive(Parser)]
@@ -16,8 +18,8 @@ struct Args {
     address: String,
     #[arg(short, long, default_value="8000")]
     port: u16,
-    #[arg(short, long)]
-    name: String,
+    #[arg(long)]
+    headers: Option<Vec<String>>,
 }
 
 #[tokio::main]
@@ -25,7 +27,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let Args {
         address,
         port,
-        name
+        headers
     } = Args::parse();
     let addr = format!("{}:{}", address, port)
         .to_socket_addrs()?
@@ -43,16 +45,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         // Use an adapter to access something implementing `tokio::io` traits as if they implement
         // `hyper::rt` IO traits.
         let io = TokioIo::new(stream);
-        let name = name.clone();
+        let headers = headers.clone();
 
         // Spawn a tokio task to serve multiple connections concurrently
         tokio::task::spawn(async move {
             // Finally, we bind the incoming connection to our `hello` service
             if let Err(err) = http1::Builder::new()
                 // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service_fn(|_| async {
-                    let res = format!("Hello from {}", name);
-                    Ok(Response::new(Full::<Bytes>::from(res))) as Result<_, Infallible>
+                .serve_connection(io, service_fn(|req: Request<Incoming>| async {
+                    let len = req.headers().get("Content-Length")
+                        .map(|v| v.clone())
+                        .or(HeaderValue::from_str("0").ok())
+                        .unwrap();
+
+                    let mut res = Response::builder()
+                        .header("Content-Length", len);
+
+                    if let Some(headers) = &headers {
+                        for header in headers {
+                            let hs: Vec<&str> = header
+                                .split_terminator(":")
+                                .map(|s| s.trim())
+                                .collect();
+                            res = res.header(hs[0], hs[1]);
+                        }   
+                    }
+
+                    res.body(req.into_body())
                 }))
                 .await
             {
