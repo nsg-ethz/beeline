3fd7fb4effe05154d7e04f0e11b5d37871c861c9
Laurin Brandner <mail@laurinbrandner.ch>
2024-09-02 10:18:35 +0200

    script: debug add backend arg

--- script/debug.sh
+++ script/debug.sh
@@ -1,3 +1,14 @@
 ROOT=$(dirname "$(readlink -f "$0")")
+BACKEND="1"
 
-curl -vvv -d @${ROOT}/../res/debug/payload-8192B.json http://10.0.5.1:3000/server1
+# Parse arguments
+while getopts "b:" opt; do
+    case $opt in
+        b ) BACKEND=${OPTARG} ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            ;;
+    esac
+done
+
+curl -vvv -d @${ROOT}/../res/debug/payload-8192B.json http://10.0.5.1:3000/server${BACKEND}
