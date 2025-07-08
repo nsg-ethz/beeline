b68742a15e74134f03346ff5e82b2a2b2cb1a7a8
Laurin Brandner <mail@laurinbrandner.ch>
2025-07-08 15:44:16 +0200

    script: optional report

--- script/bench.sh
+++ script/bench.sh
@@ -8,8 +8,10 @@ COLOR_OFF='\033[0m' # No Color
 BENCH=$1
 shift 1
 
+WRITE_REPORT=false
+
 # Parse arguments
-while getopts "c:e:f:t:n:p:s:" opt; do
+while getopts "c:e:f:t:n:p:rs:" opt; do
     case $opt in
         e ) ENV=${OPTARG} ;;
         f ) FROM=${OPTARG} ;;
@@ -17,7 +19,9 @@ while getopts "c:e:f:t:n:p:s:" opt; do
         n ) NAME=${OPTARG} ;;
         c ) CONFIG=${OPTARG} ;;
         p ) PROXY=${OPTARG} ;;
+        r ) WRITE_REPORT=true ;;
         s ) SCRIPT=${OPTARG} ;;
+
         \?)
             echo "Invalid option: -$OPTARG"
             ;;
@@ -35,6 +39,10 @@ for i in $(seq ${FROM} ${TO} ) ; do
     REPORT=${SUMMARY_DIR}/${PROXY}-k6-e${i}-full.csv
     SUMMARY=${SUMMARY_DIR}/${PROXY}-k6-e${i}-summary.json
 
+    if [ "${WRITE_REPORT}" = true ]; then
+        REPORT_OPT="--out csv=>(grep -e metric_name,timestamp -e http_req_duration > ${REPORT})"
+    fi
+
     case ${BENCH} in
         sm)
             ssh -t moonshine "source ~/.profile && ${ENV} ${ROOT}/sm.sh up -c ${ROOT}/../${CONFIG} -n ${NAME} -p ${PROXY} -e ${i}"
@@ -47,7 +55,8 @@ for i in $(seq ${FROM} ${TO} ) ; do
             elif [[ "${CONFIG}" == *ssm* ]]; then
                 K6_SCRIPT="${ROOT}/../${SCRIPT} -e PAYLOAD_SIZE=100 -e RATE=5000  -e URL=http://moonshine:8080"
             fi
-            k6 run ${K6_SCRIPT} --no-thresholds --out csv=>(grep -e metric_name,timestamp -e http_req_duration > ${REPORT}) --summary-export ${SUMMARY}
+
+            k6 run ${K6_SCRIPT} --no-thresholds ${REPORT_OPT} --summary-export ${SUMMARY}
 
             ssh -t moonshine "source ~/.profile && ${ENV} ${ROOT}/sm.sh down -c ${ROOT}/../${CONFIG} -n ${NAME} -p ${PROXY} -e ${i}"
             ;;
@@ -56,7 +65,7 @@ for i in $(seq ${FROM} ${TO} ) ; do
             ${ROOT}/mb.sh up -c ${ROOT}/../${CONFIG} -n ${NAME} -p ${PROXY} -e ${i} -m
             echo -e "${COLOR_YELLOW}Starting epoch ${i}, summary: ${SUMMARY}${COLOR_OFF}"
 
-            k6 run ${SCRIPT} -e PAYLOAD_SIZE=100 -e RATE=10000 -e URL=http://localhost:8080 --no-thresholds --summary-export ${SUMMARY}
+            k6 run ${SCRIPT} -e PAYLOAD_SIZE=100 -e RATE=10000 -e URL=http://localhost:8080 --no-thresholds ${REPORT_OPT} --summary-export ${SUMMARY}
 
             ${ROOT}/mb.sh down -c ${ROOT}/../${CONFIG} -n ${NAME} -p ${PROXY} -e ${i} -m
             ;;
