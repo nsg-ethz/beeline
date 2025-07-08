2e1c820752594a27e29c4cf645ab7ea51ed73c3c
Laurin Brandner <mail@laurinbrandner.ch>
2025-07-08 09:38:59 +0200

    beeline: remove debugging code

--- beeline/src/bpf/filter/chain.bpf.c
+++ beeline/src/bpf/filter/chain.bpf.c
@@ -3,8 +3,6 @@ static __always_inline enum pr_action _pipeline(struct sk_msg_md *msg, struct pi
     bool is_downstream = (ikey->remote.ip4 == ip4 && ikey->remote.port == port);
     enum pr_action res = PR_DROP;
 
-    bpf_log("before ctx->dest=(%d, %d)", ctx->dest.ip4, ctx->dest.port);
-
     if (is_downstream) {
         {downstream}
     }
@@ -14,8 +12,6 @@ static __always_inline enum pr_action _pipeline(struct sk_msg_md *msg, struct pi
         enum pr_action res = forward_us_conn(ikey, ctx);
         if (res == PR_DROP) return PR_DROP;
     }
-
-    bpf_log("after ctx->dest=(%d, %d)", ctx->dest.ip4, ctx->dest.port);
 
     return PR_PASS;
 }
