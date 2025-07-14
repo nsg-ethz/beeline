8c1ba9ee03e578d2344c0444c6dbc61d783202c7
Laurin Brandner <mail@laurinbrandner.ch>
2025-07-14 16:31:53 +0200

    script: fix exp/sn

--- script/exp/sn.sh
+++ script/exp/sn.sh
@@ -13,6 +13,6 @@ while getopts "f:t:n:" opt; do
     esac
 done
 
-script/bench.sh sm -e "BEELINE_CONFIG=config/beeline/sn.yaml" -c docker/sn-beeline.yaml -n ${NAME} -p beeline -s k6/sn-compose-post.js -f ${FROM} -t ${TO}
-script/bench.sh sm -c docker/sn-envoy.yaml -n ${NAME} -p envoy -s k6/sn-compose-post.js -f ${FROM} -t ${TO}
-script/bench.sh sm -c docker/sn-envoy-accelerated.yaml -n ${NAME} -p baseline -s k6/sn-compose-post.js -f ${FROM} -t ${TO}
+script/bench.sh sm -e "BEELINE_CONFIG=config/beeline/sn.yaml" -c docker/sn-beeline.yaml -n ${NAME} -p beeline -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -r
+script/bench.sh sm -c docker/sn-envoy.yaml -n ${NAME} -p envoy -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -r
+script/bench.sh sm -c docker/sn-envoy-accelerated.yaml -n ${NAME} -p baseline -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -r
