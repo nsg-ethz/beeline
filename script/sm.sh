50f4ceaff4c1d4c6ed021d758fbfd53a6a6a3eea
Laurin Brandner <mail@laurinbrandner.ch>
2025-07-15 15:37:28 +0200

    ms: fix envoy rcab

--- script/sm.sh
+++ script/sm.sh
@@ -78,15 +78,14 @@ case ${ACTION} in
             cd ${ROOT}/..
             CONFIG=${ROOT}/../${BEELINE_CONFIG} cargo b -r -p beeline
             sudo -b systemd-run -q --scope -u sm-proxy --slice beeline.slice ${PROXY_BIN} -c ${ROOT}/../${BEELINE_CONFIG}
-
-            sleep 5
             echo -e "${COLOR_GREEN}Launched beeline${COLOR_OFF}"
         elif [ "${PROXY}" = "baseline" ]; then
             PROXY_BIN=${ROOT}/../target/release/${PROXY}
             sudo -b systemd-run -q --scope -u sm-proxy --slice beeline.slice ${PROXY_BIN} -a 172.17.0.1
-            sleep 5
             echo -e "${COLOR_GREEN}Launched baseline${COLOR_OFF}"
         fi
+
+        sleep 5
 
         # populate dbs with data
         if [[ "${DOCKER_CONFIG}" == *sn* ]]; then
