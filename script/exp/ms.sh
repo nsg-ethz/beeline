722f4b9529857dde501ff19953b94cc1806e2141
Laurin Brandner <mail@laurinbrandner.ch>
2025-07-15 16:55:38 +0200

    ms: add envoy accelerated

--- script/exp/ms.sh
+++ script/exp/ms.sh
@@ -15,4 +15,5 @@ done
 
 script/bench.sh sm -e "BEELINE_CONFIG=config/beeline/ms.yaml" -c docker/ms-beeline.yaml -n ${NAME} -p beeline -s k6/ms-compose-review.js -f ${FROM} -t ${TO} -r
 script/bench.sh sm -c docker/ms-envoy.yaml -n ${NAME} -p envoy -s k6/ms-compose-review.js -f ${FROM} -t ${TO} -r
-script/bench.sh sm -c docker/ms-envoy-accelerated.yaml -n ${NAME} -p baseline -s k6/ms-compose-review.js -f ${FROM} -t ${TO} -r
+script/bench.sh sm -c docker/ms-envoy.yaml -n ${NAME} -p baseline -s k6/ms-compose-review.js -f ${FROM} -t ${TO} -r
+script/bench.sh sm -c docker/ms-envoy-accelerated.yaml -n ${NAME} -p envoy -s k6/ms-compose-review.js -f ${FROM} -t ${TO} -r
--- script/exp/sn.sh
+++ script/exp/sn.sh
@@ -15,4 +15,5 @@ done
 
 script/bench.sh sm -e "BEELINE_CONFIG=config/beeline/sn.yaml" -c docker/sn-beeline.yaml -n ${NAME} -p beeline -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -r
 script/bench.sh sm -c docker/sn-envoy.yaml -n ${NAME} -p envoy -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -r
-script/bench.sh sm -c docker/sn-envoy-accelerated.yaml -n ${NAME} -p baseline -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -r
+script/bench.sh sm -c docker/sn-envoy.yaml -n ${NAME} -p baseline -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -r
+script/bench.sh sm -c docker/sn-envoy-accelerated.yaml -n ${NAME} -p envoy -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -r
