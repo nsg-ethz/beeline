2d615e5a9ddd88325024f3761a686d24201f80c9
Laurin Brandner <mail@laurinbrandner.ch>
2025-07-30 15:03:20 +0200

    script: add mog

---
+++ script/exp/mo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Parse arguments
+while getopts "f:t:n:" opt; do
+    case $opt in
+        f ) FROM=${OPTARG} ;;
+        t ) TO=${OPTARG} ;;
+        n ) NAME=${OPTARG} ;;
+
+        \?)
+            echo "Invalid option: -$OPTARG"
+            ;;
+    esac
+done
+
+script/bench.sh sm -e "PROXY_CONFIG=sn.yaml" -c docker/sn-beeline.yaml -n ${NAME} -p beeline -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -m 1
+script/bench.sh sm -e "PROXY_CONFIG=sn.yaml" -c docker/sn-envoy.yaml -n ${NAME} -p envoy -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -m 1
+script/bench.sh sm -e "PROXY_CONFIG=sn.yaml" -c docker/sn-envoy.yaml -n ${NAME} -p envoy_l4fp -s k6/sn-compose-post.js -f ${FROM} -t ${TO} -m 1
