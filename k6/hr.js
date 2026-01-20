5b34de4336c71841e95a4cac194623be14b24387
Laurin Brandner <mail@laurinbrandner.ch>
2026-01-20 16:58:13 +0100

    k6: clean up hr

--- k6/hr.js
+++ k6/hr.js
@@ -21,7 +21,7 @@ export const options = {
 
 const dest = __ENV.URL || "https://moonshine:9991";
 
-function getRecommendations() {
+export default () => {
     const args = ["dis", "rate", "price"];
     var require = args[randomIntBetween(0, args.length - 1)];
 
@@ -30,6 +30,7 @@ function getRecommendations() {
 
     const params = {
         tags: { name: "recommendations" },
+        timeout: "3s",
     };
     const res = http.get(
         `${dest}/recommendations?require=${require}&lat=${lat}&lon=${lon}`,
@@ -39,56 +40,4 @@ function getRecommendations() {
     return check(res, {
         "status is 200": (r) => r.status === 200,
     });
-}
-
-function reserve() {
-    const userID = randomIntBetween(0, 500);
-    const userName = `Cornell_${userID}`;
-    const userPassword = `${userID}${userID}${userID}${userID}${userID}${userID}${userID}${userID}${userID}${userID}`;
-
-    const hotelID = randomIntBetween(1, 80);
-    const startDay = randomIntBetween(9, 23);
-    const endDay = randomIntBetween(startDay + 1, 24);
-
-    const startDate = `2015-04-${startDay < 10 ? "0" : ""}${startDay}`;
-    const endDate = `2015-04-${endDay < 10 ? "0" : ""}${endDay}`;
-
-    const lat = 38.0235 + (randomIntBetween(0, 481) - 240.5) / 1000.0;
-    const lon = -122.095 + (randomIntBetween(0, 325) - 157.0) / 1000.0;
-
-    const params = {
-        tags: { name: "reserve" },
-    };
-    const url = `${dest}/reservation?inDate=${startDate}&outDate=${endDate}&lat=${lat}&lon=${lon}&hotelId=${hotelID}&customerName=${userName}&username=${userID}&password=${userPassword}&number=1`;
-    const res = http.get(url, params);
-
-    return check(res, {
-        "status is 200": (r) => r.status === 200,
-    });
-}
-
-function searchHotel() {
-    const startDay = randomIntBetween(9, 23);
-    const endDay = randomIntBetween(startDay + 1, 24);
-
-    const startDate = `2015-04-${startDay < 10 ? "0" : ""}${startDay}`;
-    const endDate = `2015-04-${endDay < 10 ? "0" : ""}${endDay}`;
-
-    const lat = 38.0235 + (randomIntBetween(0, 481) - 240.5) / 1000.0;
-    const lon = -122.095 + (randomIntBetween(0, 325) - 157.0) / 1000.0;
-
-    const params = {
-        tags: { name: "search" },
-        timeout: "3s",
-    };
-    const url = `${dest}/hotels?inDate=${startDate}&outDate=${endDate}&lat=${lat}&lon=${lon}`;
-    const res = http.get(url, params);
-
-    return check(res, {
-        "status is 200": (r) => r.status === 200,
-    });
-}
-
-export default () => {
-    getRecommendations();
 };
